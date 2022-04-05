45232fc2b1520f82c2f5377739444dcdff5189c2
shusong <763455318@qq.com>
2022-04-05 11:00:27 +0800

    测试自动格式化

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,10 @@
   </div>
 </template>
 <script>
-import { ref } from "vue";
+import { ref } from "vue"
 export default {
   setup() {
-    const count = ref(0);
+    const count = ref(0)
     return {
       count,
     };
